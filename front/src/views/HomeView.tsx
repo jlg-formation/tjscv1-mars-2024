2dcd197f0ff73430b5b867a0c3ded26397fe133c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-28 09:55:32 +0100

    dev: added css for button

--- front/src/views/HomeView.tsx
+++ front/src/views/HomeView.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+
 function HomeView() {
-  return <p>HomeView works !</p>;
+  return (
+    <main className="flex grow flex-col items-center gap-2">
+      <h1 className="text-4xl font-bold">Gérer efficacement votre stock !</h1>
+      <Link to="/stock" className="btn btn-primary">
+        Voir le stock
+      </Link>
+      <button disabled className="btn btn-primary">
+        Voir le stock
+      </button>
+      <button className="btn">Voir le stock</button>
+      <button disabled className="btn">
+        Voir le stock
+      </button>
+    </main>
+  );
 }
 
 export default HomeView;
