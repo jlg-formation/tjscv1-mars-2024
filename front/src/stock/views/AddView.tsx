7229df12e52a356370f79bd858ff24b54dbfb690
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-28 12:19:47 +0100

    dev: tailwind for table

--- front/src/stock/views/AddView.tsx
+++ front/src/stock/views/AddView.tsx
@@ -4,6 +4,25 @@ function AddView() {
   return (
     <Main>
       <h1>Ajouter un article</h1>
+      <form>
+        <label>
+          <span>Nom</span>
+          <input type="text" />
+          <span className="error"></span>
+        </label>
+        <label>
+          <span>Prix</span>
+          <input type="number" />
+          <span className="error"></span>
+        </label>
+        <label>
+          <span>Quantité</span>
+          <input type="number" />
+          <span className="error"></span>
+        </label>
+        <div className="error"></div>
+        <button>Ajouter</button>
+      </form>
     </Main>
   );
 }
--- front/src/stock/views/ListView.tsx
+++ front/src/stock/views/ListView.tsx
@@ -1,17 +1,28 @@
 import { Link } from 'react-router-dom';
 import Main from '../../widgets/Main';
+import { useState } from 'react';
 
 function ListView() {
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleRefresh = () => {
+    setErrorMsg('coucou');
+  };
+
   return (
     <Main>
       <h1>Liste des articles</h1>
       <div>
-        <nav>
-          <button>Refresh</button>
-          <Link to="add">Ajouter</Link>
-          <button>Supprimer</button>
+        <nav className="flex gap-1">
+          <button className="btn" onClick={handleRefresh}>
+            Refresh
+          </button>
+          <Link to="add" className="btn">
+            Ajouter
+          </Link>
+          <button className="btn">Supprimer</button>
         </nav>
-        <div className="error"></div>
+        <div className="error h-8 font-bold">{errorMsg}</div>
         <table>
           <thead>
             <tr>
@@ -23,23 +34,23 @@ function ListView() {
           <tbody>
             <tr>
               <td className="name">Tournevis</td>
-              <td className="price">2.66 €</td>
-              <td className="qty">345</td>
+              <td className="price number">2.66 €</td>
+              <td className="qty number">345</td>
             </tr>
             <tr>
               <td className="name">Pelle</td>
-              <td className="price">12.66 €</td>
-              <td className="qty">1345</td>
+              <td className="price number">12.66 €</td>
+              <td className="qty number">1345</td>
             </tr>
             <tr>
               <td className="name">Pioche</td>
-              <td className="price">32.00 €</td>
-              <td className="qty">345</td>
+              <td className="price number">32.00 €</td>
+              <td className="qty number">345</td>
             </tr>
             <tr>
               <td className="name">Marteau</td>
-              <td className="price">2.66 €</td>
-              <td className="qty">12</td>
+              <td className="price number">2.66 €</td>
+              <td className="qty number">12</td>
             </tr>
           </tbody>
         </table>
