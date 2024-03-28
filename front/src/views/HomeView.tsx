81fe2b2244c9497bb100b275f9c047ae5c9af956
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-28 14:24:16 +0100

    dev: added fontawesome

--- front/src/views/HomeView.tsx
+++ front/src/views/HomeView.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 function HomeView() {
   return (
     <main className="flex flex-col items-center gap-2">
       <h1 className="text-4xl">Gérer efficacement votre stock !</h1>
       <Link to="/stock" className="btn btn-primary">
-        Voir le stock
+        <span>Voir le stock</span>
+        <FontAwesomeIcon icon={faAngleRight} />
       </Link>
     </main>
   );
