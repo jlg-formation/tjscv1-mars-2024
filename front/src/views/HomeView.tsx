973edc450213dfd68b0c554e03778a72f532db00
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-28 10:27:30 +0100

    dev: added center in homeview

--- front/src/views/HomeView.tsx
+++ front/src/views/HomeView.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function HomeView() {
   return (
-    <main className="flex grow flex-col items-center gap-2">
+    <main className="flex flex-col items-center gap-2">
       <h1 className="text-4xl">Gérer efficacement votre stock !</h1>
       <Link to="/stock" className="btn btn-primary">
         Voir le stock
