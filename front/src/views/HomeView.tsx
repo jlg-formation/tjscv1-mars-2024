42fb72a64b52bccda2c0bc9575622eb6569e0a0c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-29 10:13:12 +0100

    dev: h1

--- front/src/views/HomeView.tsx
+++ front/src/views/HomeView.tsx
@@ -5,7 +5,7 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 function HomeView() {
   return (
     <main className="flex flex-col items-center gap-2">
-      <h1 className="text-4xl">Gérer efficacement votre stock !</h1>
+      <h1 className="text-4xl">Gérer efficacement votre stock&nbsp;!</h1>
       <Link to="/stock" className="btn btn-primary">
         <span>Voir le stock</span>
         <FontAwesomeIcon icon={faAngleRight} />
