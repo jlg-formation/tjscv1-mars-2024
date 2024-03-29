8487ac6ce602d2e820875b05fc3ea5e444ac59e0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-29 15:08:34 +0100

    dev: added not found html

--- front/src/views/NotFoundView.tsx
+++ front/src/views/NotFoundView.tsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom';
+import Main from '../widgets/Main';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
 export default function NotFoundView() {
-  return <p>NotFound works!</p>;
+  return (
+    <Main>
+      <h1>Page non trouvée... 😭</h1>
+      <Link to="/" className="btn btn-primary">
+        <FontAwesomeIcon icon={faHome} />
+        <span>Accueil</span>
+      </Link>
+    </Main>
+  );
 }
